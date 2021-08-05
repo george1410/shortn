5310b11b304a50228e2cace87cd1278de8528ce2
George McCarron <hello@georgemccarron.com>
2021-08-05 22:44:08 +0100

    Wire up pagination to API

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,4 +1,5 @@
 import { Container, Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import axios from 'axios';
 import { useState } from 'react';
 import CreateForm from '../components/CreateForm';
 import Header from '../components/Header';
@@ -6,8 +7,23 @@ import Pagination from '../components/Pagination';
 import TableRow from '../components/TableRow';
 import Url from '../models/Url';
 
-export default function Home({ urls }) {
+const PAGE_SIZE = 3;
+
+export default function Home({ urls: initialUrls }) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [urls, setUrls] = useState(initialUrls);
+
+  const handleChangePage = async (newPage) => {
+    setCurrentPage(newPage);
+    const { data } = await axios.get('/api/urls', {
+      params: {
+        limit: PAGE_SIZE,
+        offset: (newPage - 1) * PAGE_SIZE,
+      },
+    });
+    console.log(data);
+    setUrls(data);
+  };
 
   return (
     <>
@@ -34,9 +50,9 @@ export default function Home({ urls }) {
         <Pagination
           currentPage={currentPage}
           totalPages={3}
-          onPrev={() => setCurrentPage(currentPage - 1)}
-          onNext={() => setCurrentPage(currentPage + 1)}
-          onSelectPage={(page) => setCurrentPage(page)}
+          onPrev={async () => await handleChangePage(currentPage - 1)}
+          onNext={async () => handleChangePage(currentPage + 1)}
+          onSelectPage={async (page) => await handleChangePage(page)}
         />
       </Container>
     </>
@@ -44,7 +60,7 @@ export default function Home({ urls }) {
 }
 
 export const getServerSideProps = async (context) => {
-  const limit = 20,
+  const limit = PAGE_SIZE,
     offset = 0;
 
   const urls = await Url.findAll({
