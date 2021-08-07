c3f78c29dbfe498ac9c2bf46c3041edf03886496
George McCarron <hello@georgemccarron.com>
2021-08-07 19:39:29 +0100

    Storybook setup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,24 @@
+import { ThemeProvider } from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react';
+const theme = {};
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: 'centered',
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <ChakraProvider>
+        <Story />
+      </ChakraProvider>
+    </ThemeProvider>
+  ),
+];
