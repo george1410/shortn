d918b4d985671a55d4cec3ba066452107fdba797
George McCarron <hello@georgemccarron.com>
2021-08-08 13:24:27 +0100

    Add TableRow stories

---
+++ stories/TableRow.stories.js
@@ -0,0 +1,40 @@
+import { Table } from '@chakra-ui/react';
+import TableRow from '../src/components/TableRow';
+
+const config = {
+  title: 'Components/TableRow',
+  component: TableRow,
+  argTypes: {
+    loading: { control: { type: 'boolean' } },
+  },
+  decorators: [
+    (Story) => (
+      <Table width='900px'>
+        <Story />
+      </Table>
+    ),
+  ],
+};
+export default config;
+
+const template = (args) => <TableRow {...args} />;
+
+export const Loading = template.bind({});
+Loading.args = {
+  loading: true,
+  url: {
+    shortUrl: 'blog',
+    originalUrl: 'https://georgemccarron.com/blog/posts/hello-world',
+    clicks: 10,
+  },
+};
+
+export const Loaded = template.bind({});
+Loaded.args = {
+  loading: false,
+  url: {
+    shortUrl: 'blog',
+    originalUrl: 'https://georgemccarron.com/blog/posts/hello-world',
+    clicks: 10,
+  },
+};
